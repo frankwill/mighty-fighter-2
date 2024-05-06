96be3c764e7cf7677e862460c9e218a2b95b185f
Frank Willian da Silva Barbosa <frankwsb@uninove.br>
2024-05-06 14:17:22 -0300

    Implementando modo SPA para as rotas da página

---
+++ src/assets/js/router.js
@@ -0,0 +1,26 @@
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
+};
+
+const routes = {
+  "/": "/src/pages/index.html",
+  "/lutadores": "/src/pages/lutadores.html",
+  "/social": "/src/pages/social.html",
+  "/changelog": "/src/pages/changelog.html",
+  "/login": "/src/pages/login.html",
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main-page").innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();

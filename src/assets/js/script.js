677467c3bad11a7253328a062c720350e96b688e
Frank Willian da Silva Barbosa <frankwsb@uninove.br>
2024-05-07 18:00:49 -0300

    Ajustes no poster e adicionando JS na nav

---
+++ src/assets/js/script.js
@@ -0,0 +1,7 @@
+const navigationLinks = document.querySelectorAll('.navigation__links')
+
+navigationLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    // link.innerHTML.style.color = "red"
+  })
+})

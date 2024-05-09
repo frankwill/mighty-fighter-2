35efc9e0eff2000a69a5ef5ed16871f52d911870
Frank Willian da Silva Barbosa <frankwsb@uninove.br>
2024-05-09 16:13:40 -0300

    Navbar e actives arrumados

--- src/assets/js/navigationLinks.js
+++ src/assets/js/navigationLinks.js
@@ -1,10 +1,23 @@
 export default function navigationLinks() {
   const navigationLinks = document.querySelectorAll(".navigation__links")
+  const arrayNavigationLinks = Array.from(navigationLinks)
 
   navigationLinks.forEach((link) => {
     link.addEventListener("click", () => {
       navigationLinks.forEach((allLinks) => allLinks.classList.remove("active"))
       link.classList.add("active")
     })
+  })
+
+  const linkLutadores = arrayNavigationLinks.find(link => link.getAttribute("href") === "/lutadores")
+  window.navigation.addEventListener("navigate", (event) => {  
+    navigationLinks.forEach(link => {
+      if(link.classList.contains("active")) {
+        link.classList.remove("active")
+      }
+    })
+    if (event.destination.url === "http://127.0.0.7:5500/lutadores") {
+      linkLutadores.classList.add("active")
+    }
   })
 }

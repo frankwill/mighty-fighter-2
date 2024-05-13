985238538791a7d2f4e92b40ac03f4185b609481
Frank Willian da Silva Barbosa <frankwsb@uninove.br>
2024-05-13 15:48:43 -0300

    Oágina changelogs ok

--- public/pages/changelog.php
+++ public/pages/changelog.php
@@ -1,57 +1,52 @@
-<section class="container d-flex flex-column align-items-center row-gap-5">
+<?php
+
+use frankwsb\mf2\Model\Changelog;
+use frankwsb\mf2\Repository\ChangelogRepository;
+
+$dbPath = __DIR__ . "/../../banco.sqlite";
+$pdo = new PDO("sqlite:$dbPath");
+
+require_once dirname(__FILE__) . '/../../vendor/autoload.php';
+
+$repository = new ChangelogRepository($pdo);
+
+$changelogsData = $repository->all();
+
+$changelogObject = array_map(function($changelogsData) {
+
+  $classeChangelog = new Changelog(
+    $changelogsData['title'],
+    new \DateTimeImmutable($changelogsData['date']),
+    $changelogsData['description']
+  );
+
+  return $classeChangelog;
+}, $changelogsData);
+
+?>
+
+<section class="change container d-flex flex-column align-items-center row-gap-5">
   <span class="text-h2">CHANGELOG</span>
   <span class="text-body text-center">
-    Explore as últimas atualizações de Mighty Fighter 2 com nosso changelog mais
-    recente, onde detalhamos meticulosamente todas as correções de bugs
-    implementadas, melhorias de desempenho e ajustes de equilíbrio que foram
-    cuidadosamente incorporados para elevar sua experiência de jogo a novos
-    patamares de epicidade!
+    Confira o changelog mais recente do nosso jogo: correções de bugs, melhorias de desempenho e ajustes de equilíbrio para tornar sua experiência ainda mais épica!
   </span>
-  <hr class="line-divider" />
-  <div class="changelog d-flex flex-column align-items-center row-gap-3">
-    <div class="d-flex align-items-center column-gap-3">
-      <span class="text-h5">Notas do patch - Atualização v01.2</span>
+  <hr class="line-divider">
+  <?php foreach($changelogObject as $changelog) { ?>
+  <div class="changelog d-flex flex-column align-items-start row-gap-3">
+    <div class="d-flex align-self-center column-gap-3">
+      <span class="text-h5"><?php echo $changelog->getTitle() ?></span>
       <button class="btn-edit">
         <span class="material-icons-outlined color-primary-dark">edit</span>
       </button>
     </div>
-    <span class="text-caption">01 de abril, 2024</span>
+    <span class="text-caption align-self-center"><?php echo $changelog->getDate() ?></span>
+    <?php $listItensChangelog = explode(";", $changelog->getDescription()); ?>
     <ul class="text-body">
-      <li>Adicionados 2 novos personagens jogáveis: C. Tonaldo e Aren.</li>
-      <li>
-        Ajustes de balanceamento nos ataques especiais de todos os personagens
-        para uma experiência de jogo mais equilibrada.
-      </li>
-      <li>
-        Corrigidos diversos bugs, incluindo problemas de colisão de sprites e
-        falhas de ataques.
-      </li>
-      <li>
-        Aprimoramentos de desempenho para garantir uma jogabilidade mais fluida.
-      </li>
+      <?php foreach($listItensChangelog as $item) { ?>
+      <li><?php echo $item ?></li>
+      <?php } ?>
     </ul>
   </div>
-  <div class="changelog d-flex flex-column align-items-center row-gap-3">
-    <div class="d-flex align-items-center column-gap-3">
-      <span class="text-h5">Notas do patch - Atualização v01.1</span>
-      <button class="btn-edit">
-        <span class="material-icons-outlined color-primary-dark">edit</span>
-      </button>
-    </div>
-    <span class="text-caption">01 de abril, 2024</span>
-    <ul class="text-body">
-      <li>Adicionados 2 novos personagens jogáveis: C. Tonaldo e Aren.</li>
-      <li>
-        Ajustes de balanceamento nos ataques especiais de todos os personagens
-        para uma experiência de jogo mais equilibrada.
-      </li>
-      <li>
-        Corrigidos diversos bugs, incluindo problemas de colisão de sprites e
-        falhas de ataques.
-      </li>
-      <li>
-        Aprimoramentos de desempenho para garantir uma jogabilidade mais fluida.
-      </li>
-    </ul>
-  </div>
+  <?php } ?>
+  <a href="/changelog" onclick="route()" id="ver-mais-changelog" class="btn flat text-button color-neutral-light text-decoration-none text-center">Ver mais</a>
 </section>
